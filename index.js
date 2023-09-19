a07a14524bc22a6a96d5a0fc07ddf4f1b2de23a2
mako-rzm <141319571+mako-rzm@users.noreply.github.com>
2023-09-19 20:26:17 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+//let a = 100;
+
+//if ( a < 50 ){
+//    console.log('a less than 50');
+//}
+
+//else if ( a > 20 ) {
+ //   console.log('a is greater than 20');
+//}
+
+//else {
+
+ //   console.log ('null');
+//}
+
+//----------------------------------------------------
+
+//let text = მარიამ;
+
+//if ( text == მარიამ) {
+  ///  console.log('true');
+//}
+
+//else {
+//    console.log('false');
+//}
+
+//--------------------------------------------------------
+
+//let text = "მარიამ";
+//switch (text){
+    //case "მარიამ":
+       // document.write('true')
+       // break;
+    //default:
+     //   document.write('false')
+      //  break;
+//}
+
+//----------------------------------------------------------
+
